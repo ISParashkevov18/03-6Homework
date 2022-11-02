a5594aaee12c294516d07663ec33398e81235abd
ISParashkevov18 <ISParashkevov18@codingburgas.bg>
2022-11-02 15:07:31 +0200

    Initial commit

---
+++ src/test/java/com/example/kebapche/KebapcheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kebapche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KebapcheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
